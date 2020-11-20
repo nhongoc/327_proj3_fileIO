c8a28bdf60d879c825650dc02ae082d29f15a2c9
Laura Slayton <laura.slayton.19@cnu.edu>
2020-11-20 13:56:16 -0500

    done

--- FileIO.cpp
+++ FileIO.cpp
@@ -11,14 +11,32 @@
 
 using namespace std;
 
-int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
-{
-	//TODO fill in
+int KP_FileIO::getFileContents(const std::string &filename,
+		std::string &contents) {
+	fstream stream;
+	stream.open(filename.c_str(), ios::in);
+	if (!stream.is_open()) {
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+	string line;
+	while (getline(stream, line)) {
+		contents += line;
+	}
+	stream.close();
+	return SUCCESS;
 }
 
-int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
-{
-	//TODO fill in
+int KP_FileIO::writeVectortoFile(const std::string filename,
+		std::vector<std::string> &myEntryVector) {
+	fstream stream;
+	stream.open(filename.c_str(), ios::out);
+	if (!stream.is_open()) {
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+	for (int i = 0; i < myEntryVector.size(); i++) {
+		stream << myEntryVector[i] << endl;
+	}
+	stream.close();
+	return SUCCESS;
 }
-
 
